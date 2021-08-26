c756ff42b48cf25b3ca41437da5342b209e61abd
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-08-25 23:30:49 -0300

    Project done, not API

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ const App = () => {
             </>
           )}
         />
-        <Route path="" exact render={TaskDetails}/>
+        <Route path="/:taskTitle" exact component={TaskDetails} />
       </div>
     </ Router>
   )
--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
 import Button from './Button'
 
+import './TaskDetails.css'
+
 const TaskDetails = () => {
+    const params = useParams()
+    const history = useHistory()
+
+    const handleBackButtonClick = () => {
+        history.goBack()
+    }
+
     return (
         <>
             <div className="back-button-container">
-                <button>Voltar</button>
+                <Button onClick={handleBackButtonClick}>Voltar</ Button>
             </div>
             <div className="task-details-container">
+                <h2>{params.taskTitle}</h2>
                 <p>
                     Lorem ipsum dolor sit amet, consectet
                 </p>
